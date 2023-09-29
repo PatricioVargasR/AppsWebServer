338a0b6dc3f8a576026b4044cd5d844db07261ea
lPatricio_VargasR <1722110158@utectulancingo.edu.mx>
2023-09-29 11:57:43 -0600

    UPDATE función read_root

--- main.py
+++ main.py
@@ -4,15 +4,36 @@
         JSON enconde contactos.csv
         save in response
 """
-
-from fastapi import FastAPI
+#from fastapi import FastAPI
+from fastapi import FastAPI, status # Importa la variable de estados
 import csv
 
 app = FastAPI()
 
-@app.get("/")
+# @app.get("/") # Asignación predeterminada
+# @app.get("/", status_code=202) # Asignación sin librería, solo poniendo el número
+# @app.get("/", status_code=status.HTTP_200_OK) # Asignación a través de la libreria, permitiendo desplegar opciones de código
+# Agrega una descripción y resumen en la documentación
+"""
+@app.get(
+        "/",
+        status_code=status.HTTP_200_OK,
+        description="Endpoint raíz de la API Contactos",
+        summary="Endpoint raíz"
+         )
+"""
+@app.get("/", status_code=status.HTTP_200_OK, summary="Endpoint raíz")
 def read_root():
 # async def root(): # Convierte la función en asincrona
+# Agregamos documentación  
+    """
+    # Endpoint raíz
+    
+    ## 1- Status codes:
+    * 200 - Código de confirmación
+    * 289 - Código de muestra
+    * 334 - Otro código de muestra
+    """
     return {"Hello":"World"}
 
 @app.get("/v1/contactos")
