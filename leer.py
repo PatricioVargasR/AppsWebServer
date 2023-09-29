110d3e0e8ca93327ac0834d253986208d6dbddfa
lPatricio_VargasR <1722110158@utectulancingo.edu.mx>
2023-09-29 11:26:11 -0600

    DELETE leer.py

--- leer.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-datos = []
-with open('contactos.csv', 'r') as file:
-    fildnames = ('nombre', 'email')
-    lector = csv.DictReader(file, fildnames)
-    for row in lector:
-        datos.append(row)
-    print(datos)
